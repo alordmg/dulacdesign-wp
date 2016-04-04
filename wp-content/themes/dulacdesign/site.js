94e25b1f34d273d045c1c7ce2fc7cf2924d6b96c
Antoine Lord <antoinelord@gmail.com>
2016-04-03 20:42:45 -0400

    Ajout de la navigation

--- wp-content/themes/dulacdesign/site.js
+++ wp-content/themes/dulacdesign/site.js
@@ -1,5 +1,12 @@
-jQuery( document ).ready( function( $ ) {
+window.onload = function() {
+  //alert('TEST');
+  var classesIconNav = document.getElementsByClassName("nav-icon");
 
-  alert('BONJOUR');
+  for (var i = 0; i < classesIconNav.length; i++) {
+      classesIconNav[i].addEventListener('click', ouvrirNavigation, false);
+  }
+}
 
-});
+function ouvrirNavigation() {
+  document.getElementById('navigation').classList.toggle('nav-ouvert');
+}
