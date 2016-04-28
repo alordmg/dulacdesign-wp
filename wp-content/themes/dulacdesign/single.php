7c0b867ff4131d378fc4bf14e6e88135f471d7bc
Antoine Lord <antoine@lesmauvaisgarcons.ca>
2016-04-27 22:46:06 -0400

    Beaucoup de stock

--- wp-content/themes/dulacdesign/single.php
+++ wp-content/themes/dulacdesign/single.php
@@ -1,21 +1,6 @@
 <?php
-/**
- * The Template for displaying all single posts
- *
- * Methods for TimberHelper can be found in the /lib sub-directory
- *
- * @package  WordPress
- * @subpackage  Timber
- * @since    Timber 0.1
- */
 
 $context = Timber::get_context();
-$post = Timber::query_post();
-$context['post'] = $post;
-$context['comment_form'] = TimberHelper::get_comment_form();
-
-if ( post_password_required( $post->ID ) ) {
-	Timber::render( 'single-password.twig', $context );
-} else {
-	Timber::render( array( 'single-' . $post->ID . '.twig', 'single-' . $post->post_type . '.twig', 'single.twig' ), $context );
-}
+$context['post'] = new TimberPost(); // It's a new TimberPost object, but an existing post from WordPress.
+Timber::render('single.twig', $context);
+?>
